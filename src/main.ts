661b9af691111e9c8d69a2e3cca110b3dcc13905
bockyonmars <user@Bockys-MacBook-Pro.local>
2026-04-24 18:42:22 +0100

    Fix cookie parser import

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
