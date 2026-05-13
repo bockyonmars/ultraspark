892f85d2c67ee98437218a59852673a69a16b803
bockyonmars <user@Bockys-MacBook-Pro.local>
2026-05-13 02:23:03 +0100

    Remove accidental frontend files from prisma folder

--- prisma/Desktop/frontend 3/src/components/services/ServicesGrid.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-type ServicesGridProps = {
-  children: ReactNode;
-};
-
-export function ServicesGrid({ children }: ServicesGridProps) {
-  return <div className="framer-services-list">{children}</div>;
-}
